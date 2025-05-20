3a3b6bf0567fc05ab6ae7e614390a26f0b518b04
sandeepknl2 <161841885+sandeepknl2@users.noreply.github.com>
2025-05-20 14:39:24 +0530

    Add files via upload

---
+++ CoursePlatformApplication.java
@@ -0,0 +1,11 @@
+package com.example.courseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePlatformApplication.class, args);
+    }
+}
